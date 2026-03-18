08df248c3eccca87222e20a2bdd6c61262f70ec1
Lazim Xhaferi <lazimxhaferi@Lazims-MacBook-Air.local>
2026-03-18 14:13:14 +0100

    Dummy

--- indx.js
+++ indx.js
@@ -5,3 +5,8 @@ console.log("Hello form Seeu")
 //1. git init
 // 2. git add remote origin
 // 3.git branch  -M main
+//we have pushed the code on main branch
+
+const dummy = () => {
+    console.log("dummy")
+}
